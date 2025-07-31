b27e2f7e2b4d7a7a59549dca74c148d6ff5b31f2
scopusjin <matteo.moretti19@gmail.com>
2025-07-31 10:05:57 +0200

    Update per_streamlit.py

--- per_streamlit.py
+++ per_streamlit.py
@@ -388,7 +388,7 @@ if mostra_parametri_aggiuntivi:
 
                 with col1:
                     # Checkbox con margine sopra per alzarlo
-                    st.markdown("<div style='margin-top: 6px;'>", unsafe_allow_html=True)
+                    st.markdown("<div style='margin-top: 2px;'>", unsafe_allow_html=True)
                     usa_orario_personalizzato = st.checkbox(
                         label="",
                         key=chiave_checkbox
