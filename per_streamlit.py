64593f90b342b606f09d1330acf01fb992c6b9b9
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 20:04:37 +0200

    Update per_streamlit.py

--- per_streamlit.py
+++ per_streamlit.py
@@ -307,7 +307,7 @@ with st.container():
     col1, col2, col3, col4, col5 = st.columns([1, 0.2, 1, 0.2, 1], gap="small")
 
     with col1:
-        subcol1, subcol2 = st.columns([1.5, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1.2, 0.9], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>T. rettale (°C):</div>", unsafe_allow_html=True)
         with subcol2:
@@ -316,7 +316,7 @@ with st.container():
             )
 
     with col3:
-        subcol1, subcol2 = st.columns([1.5, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1.2, 0.9], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>T. ambientale (°C):</div>", unsafe_allow_html=True)
         with subcol2:
@@ -325,7 +325,7 @@ with st.container():
             )
 
     with col5:
-        subcol1, subcol2 = st.columns([1.5, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1.2, 0.9], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>Peso (kg):</div>", unsafe_allow_html=True)
         with subcol2:
