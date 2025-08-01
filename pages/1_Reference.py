88668bcd8dbf67ed66f7d9649a415a742bb42f1e
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 14:08:46 +0200

    Create 1_Reference.py

---
+++ pages/1_Reference.py
@@ -0,0 +1,26 @@
+References:
+Henssge, C., and B. Madea. "Estimation of the time since death in the early post-mortem period." Forensic science international 144.2 (2004): 167-175.
+
+@article{henssge2004estimation,
+title={Estimation of the time since death in the early post-mortem period},
+author={Henssge, C and Madea, B},
+journal={Forensic science international},
+volume={144},
+number={2},
+pages={167--175},
+year={2004},
+publisher={Elsevier}
+}
+
+Henssge C. Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen. Z Rechtsmed. 1981;187:147-78.
+
+@article{henssge1981todeszeitschatzungen,
+title={Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen},
+author={Henssge, C},
+journal={Z Rechtsmed},
+volume={187},
+pages={147--178},
+year={1981}
+}
+
+pHp-code written and implemented 2005 by Wolf Schweitzer, MD, Institute of Legal Medicine, University of Zuerich, Switzerland - method described by Henssge C (2002) Todeszeitbestimmung an Leichen; Rechtsmedizin 12:112-131
