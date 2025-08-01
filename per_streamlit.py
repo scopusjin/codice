b4e10c19baeab4758ce89f3e65702e9b31387315
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 20:13:22 +0200

    Update per_streamlit.py

--- per_streamlit.py
+++ per_streamlit.py
@@ -304,7 +304,7 @@ with st.container():
         with subcol2:
             selettore_rigidita = st.selectbox("Rigidità cadaverica:", options=list(opzioni_rigidita.keys()), label_visibility="collapsed")
 
-    col1, col2, col3, col4, col5 = st.columns([1, 0.3, 1.3, 0.2, 1], gap="small")
+    col1, col2, col3, col4, col5 = st.columns([1, 0.2, 1.3, 0.2, 0.9], gap="small")
 
     with col1:
         subcol1, subcol2 = st.columns([1, 1.5], gap="small")
@@ -325,7 +325,7 @@ with st.container():
             )
 
     with col5:
-        subcol1, subcol2 = st.columns([1, 1.5], gap="small")
+        subcol1, subcol2 = st.columns([1, 1.7], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>Peso (kg):</div>", unsafe_allow_html=True)
         with subcol2:
