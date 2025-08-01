737390e7975e8466fb4478918d248cbdabaed66b
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 14:51:45 +0200

    Update 2_Tabelle di riferimento.py

--- pages/2_Tabelle di riferimento.py
+++ pages/2_Tabelle di riferimento.py
@@ -2,14 +2,15 @@ import streamlit as st
 
 st.title("Tabelle di riferimento")
 
-st.markdown("## Ipostasi (Mallach)")
+# --- Sezione 1: Mallach ---
+st.markdown("## Ipostasi")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Ipostasi%20(Mallach).jpeg",
     caption="Ipostasi (Mallach)",
     use_container_width=True
 )
 
-st.markdown("## Rigidità cadaverica (Mallach)")
+st.markdown("## Rigidità cadaverica")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Rigor%20(Mallach).jpeg",
     caption="Rigor (Mallach)",
@@ -20,5 +21,27 @@ st.markdown("## Metodi combinati")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Metodi%20combinati.jpeg",
     caption="Metodi combinati",
+    use_container_width=True
+)
+
+# --- Sezione 2: Tabelle Henssge ---
+st.markdown("## Fattori di correzione base")
+st.image(
+    "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Tabella%201%20henssge.png",
+    caption="Tabella 1 henssge",
+    use_container_width=True
+)
+
+st.markdown("Situazioni speciali")
+st.image(
+    "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Tabella%202%20Henssge.png",
+    caption="Tabella 2 Henssge",
+    use_container_width=True
+)
+
+st.markdown("Adattamento per peso corporeo")
+st.image(
+    "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Tabella%203%20Henssge.png",
+    caption="Tabella 3 Henssge",
     use_container_width=True
 )
