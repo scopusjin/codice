69cc99540e593d5fba1e79b34dec06af0df260a8
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 14:41:03 +0200

    Update 2_Tabelle di riferimento.py

--- pages/2_Tabelle di riferimento.py
+++ pages/2_Tabelle di riferimento.py
@@ -1,25 +1,24 @@
 import streamlit as st
 
-st.title("📊 Tabelle di riferimento medico-legale")
+st.title("Tabelle di riferimento medico-legale")
 
 st.markdown("## Ipostasi (Mallach)")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Ipostasi%20(Mallach).jpeg",
     caption="Ipostasi (Mallach)",
-    use_column_width=True
+    use_container_width=True
 )
 
 st.markdown("## Rigidità cadaverica (Mallach)")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Rigor%20(Mallach).jpeg",
     caption="Rigor (Mallach)",
-    use_column_width=True
+    use_container_width=True
 )
 
 st.markdown("## Metodi combinati")
 st.image(
     "https://raw.githubusercontent.com/scopusjin/codice/Fattore-di-correzione/immagini/Metodi%20combinati.jpeg",
     caption="Metodi combinati",
-    use_column_width=True
+    use_container_width=True
 )
-
