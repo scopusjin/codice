5c8564473a728430d9627b3f9d6cc875227f4a8d
scopusjin <matteo.moretti19@gmail.com>
2025-08-01 20:07:23 +0200

    Update per_streamlit.py

--- per_streamlit.py
+++ per_streamlit.py
@@ -304,10 +304,10 @@ with st.container():
         with subcol2:
             selettore_rigidita = st.selectbox("Rigidità cadaverica:", options=list(opzioni_rigidita.keys()), label_visibility="collapsed")
 
-    col1, col2, col3, col4, col5 = st.columns([1, 0.5, 1, 0.5, 1], gap="small")
+    col1, col2, col3, col4, col5 = st.columns([1, 0.3, 1, 0.3, 1], gap="small")
 
     with col1:
-        subcol1, subcol2 = st.columns([1, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1.2, 1], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>T. rettale (°C):</div>", unsafe_allow_html=True)
         with subcol2:
@@ -316,7 +316,7 @@ with st.container():
             )
 
     with col3:
-        subcol1, subcol2 = st.columns([1.5, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1.7, 1], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>T. ambientale (°C):</div>", unsafe_allow_html=True)
         with subcol2:
@@ -325,7 +325,7 @@ with st.container():
             )
 
     with col5:
-        subcol1, subcol2 = st.columns([1, 0.9], gap="small")
+        subcol1, subcol2 = st.columns([1, 1], gap="small")
         with subcol1:
             st.markdown("<div style='font-size: 0.88rem; padding-top: 0.4rem;'>Peso (kg):</div>", unsafe_allow_html=True)
         with subcol2:
