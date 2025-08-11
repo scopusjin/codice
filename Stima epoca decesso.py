f9d7ebe5600f517dec4c9dcfedf767c8402d9a3c
scopusjin <matteo.moretti19@gmail.com>
2025-08-11 22:58:51 +0200

    Update Stima epoca decesso.py

--- Stima epoca decesso.py
+++ Stima epoca decesso.py
@@ -506,9 +506,9 @@ with st.container():
     
 
     # 📌 1. Data e ora ispezione legale
+    st.markdown("<div style='font-size: 0.88rem;'>Data e ora dei rilievi:</div>", unsafe_allow_html=True)
     col1, col2 = st.columns(2, gap="small")
     with col1:
-        st.markdown("<div style='font-size: 0.88rem;'>Data e ora dei rilievi:</div>", unsafe_allow_html=True)
         input_data_rilievo = st.date_input("Data ispezione legale:", value=datetime.date.today(), label_visibility="collapsed")
 
     with col2:
