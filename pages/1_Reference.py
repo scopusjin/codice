50ba5ad0115e05ade61ca6548d0de07c26705530
scopusjin <matteo.moretti19@gmail.com>
2025-08-13 17:51:23 +0200

    Update 1_Reference.py

--- pages/1_Reference.py
+++ pages/1_Reference.py
@@ -1,32 +1,24 @@
+# -*- coding: utf-8 -*-
 import streamlit as st
+from references import REFERENCES_MD
 
 st.title("References")
+st.markdown(REFERENCES_MD)
 
-st.markdown("References:
-Henssge, C., and B. Madea. "Estimation of the time since death in the early post-mortem period." Forensic science international 144.2 (2004): 167-175.
 
-@article{henssge2004estimation,
-title={Estimation of the time since death in the early post-mortem period},
-author={Henssge, C and Madea, B},
-journal={Forensic science international},
-volume={144},
-number={2},
-pages={167--175},
-year={2004},
-publisher={Elsevier}
-}
+"""
+Riferimenti bibliografici per la stima dell'epoca della morte
+"""
 
-Henssge C. Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen. Z Rechtsmed. 1981;187:147-78.
+REFERENCES_MD = """
+**References**
 
-@article{henssge1981todeszeitschatzungen,
-title={Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen},
-author={Henssge, C},
-journal={Z Rechtsmed},
-volume={187},
-pages={147--178},
-year={1981}
-}
-
-pHp-code written and implemented 2005 by Wolf Schweitzer, MD, Institute of Legal Medicine, University of Zuerich, Switzerland - method described by Henssge C (2002) Todeszeitbestimmung an Leichen; Rechtsmedizin 12:112-131
-.")
-
+- Handbook of Forensic Medicine - Editor: Burkhard Madea, 2022 – Chapter 7 – *Post-mortem changes and time since death*.
+- [Swisswuff - Time of Death Calculator](https://www.swisswuff.ch/calculators/todeszeit.php)
+- Mallach HJ (1964) Zur Frage der Todeszeitbestimmung. *Berl Med* 18:577–582.
+- Potente S, Kettner M, Verhoff MA, Ishikawa T. Minimum time since death when the body has either reached or closely approximated equilibrium with ambient temperature. *Forensic Sci Int*. 2017 Dec;281:63-66. doi: 10.1016/j.forsciint.2017.09.012. Epub 2017 Oct 28. PMID: 29102846.
+- Henssge, C., and B. Madea. *Estimation of the time since death in the early post-mortem period*. *Forensic Science International* 144.2 (2004): 167-175.
+- Henssge C. *Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen*. *Z Rechtsmed*. 1981;187:147-178.
+- Henssge C (2002) *Todeszeitbestimmung an Leichen*. *Rechtsmedizin* 12:112-131.
+- PHP-code written and implemented 2005 by Wolf Schweitzer, MD, Institute of Legal Medicine, University of Zurich, Switzerland – method described by Henssge C (2002).
+"""
