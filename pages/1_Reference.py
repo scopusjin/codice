0984e2939cc5fcdf152649a7619973229b56e87c
scopusjin <matteo.moretti19@gmail.com>
2025-08-13 18:03:03 +0200

    Update 1_Reference.py

--- pages/1_Reference.py
+++ pages/1_Reference.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 
-st.title("References")
+
 
 
 
@@ -11,15 +11,16 @@ Riferimenti bibliografici per la stima dell'epoca della morte
 """
 
 REFERENCES_MD = """
-**References**
+
 
 - Handbook of Forensic Medicine - Editor: Burkhard Madea, 2022 – Chapter 7 – *Post-mortem changes and time since death*.
-- [Swisswuff - Time of Death Calculator](https://www.swisswuff.ch/calculators/todeszeit.php)
+- PHP-code written and implemented 2005 by Wolf Schweitzer, MD, Institute of Legal Medicine, University of Zurich, Switzerland – method described by Henssge C (2002) [Swisswuff - Time of Death Calculator](https://www.swisswuff.ch/calculators/todeszeit.php)
+- Otatsume M, Shinkawa N, Tachibana M, Kuroki H, Ro A, Sonoda A, Kakizaki E, Yukawa N. Technical note: Excel spreadsheet calculation of the Henssge equation as an aid to estimating postmortem interval. J Forensic Leg Med. 2024.
 - Mallach HJ (1964) Zur Frage der Todeszeitbestimmung. *Berl Med* 18:577–582.
-- Potente S, Kettner M, Verhoff MA, Ishikawa T. Minimum time since death when the body has either reached or closely approximated equilibrium with ambient temperature. *Forensic Sci Int*. 2017 Dec;281:63-66. doi: 10.1016/j.forsciint.2017.09.012. Epub 2017 Oct 28. PMID: 29102846.
+- Potente S, Kettner M, Verhoff MA, Ishikawa T. Minimum time since death when the body has either reached or closely approximated equilibrium with ambient temperature. *Forensic Sci Int*. 2017.
 - Henssge, C., and B. Madea. *Estimation of the time since death in the early post-mortem period*. *Forensic Science International* 144.2 (2004): 167-175.
 - Henssge C. *Todeszeitschatzungen durch die mathematische Beschreibung der rektalen Leichenabkuhlung unter verschiedenen Abkuhlbedingungen*. *Z Rechtsmed*. 1981;187:147-178.
 - Henssge C (2002) *Todeszeitbestimmung an Leichen*. *Rechtsmedizin* 12:112-131.
-- PHP-code written and implemented 2005 by Wolf Schweitzer, MD, Institute of Legal Medicine, University of Zurich, Switzerland – method described by Henssge C (2002).
+- - Potente S, Kettner M, Verhoff MA, Ishikawa T. Minimum time since death when the body has either reached or closely approximated equilibrium with ambient temperature. *Forensic Sci Int*. 2017.
 """
 st.markdown(REFERENCES_MD)
