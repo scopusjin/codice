048bf5782a36be1c9cb4662799083f6d1ae5a2a8
scopusjin <matteo.moretti19@gmail.com>
2025-08-23 23:02:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ import pandas as pd
 # =========================
 # Stato e costanti globali
 # =========================
-st.set_page_config(page_title="MStima Epoca della Morte", layout="centered")
+st.set_page_config(page_title="Stima Epoca della Morte", layout="centered")
 
 if "fattore_correzione" not in st.session_state:
     st.session_state["fattore_correzione"] = 1.0
