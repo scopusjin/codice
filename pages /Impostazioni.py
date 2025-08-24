d18de494b0bbf9595441420c678056e7d512b94c
scopusjin <matteo.moretti19@gmail.com>
2025-08-24 21:13:08 +0200

    Update Impostazioni.py

--- pages /Impostazioni.py
+++ pages /Impostazioni.py
@@ -1,1 +1,24 @@
+# pages/Impostazioni.py
+import streamlit as st
+
+st.set_page_config(page_title="Impostazioni", layout="centered")
+
+st.markdown("## ⚙️ Impostazioni")
+
+# Default se non c'è ancora
+if "henssge_round_minutes" not in st.session_state:
+    st.session_state["henssge_round_minutes"] = 30
+
+scelta = st.radio(
+    "Specifica con quale arrotondamento vuoi che sia fornito la stima ottenuta applicando il metodo di Henssge per il raffreddamento cadaverico",
+    ["6 minuti", "15 minuti", "30 minuti"],
+    index={6: 0, 15: 1, 30: 2}[st.session_state["henssge_round_minutes"]],
+)
+st.session_state["henssge_round_minutes"] = {
+    "6 minuti": 6,
+    "15 minuti": 15,
+    "30 minuti": 30,
+}[scelta]
+
+st.success(f"Impostato a {st.session_state['henssge_round_minutes']} minuti.")
 
