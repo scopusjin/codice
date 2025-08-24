a3526885c227a636e3c650af2270b36e9142144c
scopusjin <matteo.moretti19@gmail.com>
2025-08-24 21:04:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -517,7 +517,7 @@ def aggiorna_grafico():
     rigidita_selezionata = selettore_rigidita
 
     t_med_raff_hensge_rounded, t_min_raff_hensge, t_max_raff_hensge, t_med_raff_hensge_rounded_raw, Qd_val_check = calcola_raffreddamento(
-        Tr_val, Ta_val, T0_val, W_val, CF_val
+        Tr_val, Ta_val, T0_val, W_val, CF_va, round_minutes=30
     )
     qd_threshold = 0.2 if Ta_val <= 23 else 0.5
     raffreddamento_calcolabile = not np.isnan(t_med_raff_hensge_rounded) and t_med_raff_hensge_rounded >= 0
