964e2b3629ea162eaba7bb8c5e9ea75d36483fd7
scopusjin <matteo.moretti19@gmail.com>
2025-08-25 08:45:43 +0200

    Update raccomandazioni.py

--- pages/raccomandazioni.py
+++ pages/raccomandazioni.py
@@ -1,1 +1,18 @@
+# pages/raccomandazioni.py
+import streamlit as st
+
+st.title("Raccomandazioni per l’utilizzo dei metodi")
+
+st.markdown(
+"""
+**Il metodo di Henssge non può essere applicato nelle seguenti circostanze:**
+- Non è possibile stabilire che il luogo di rinvenimento del corpo coincida con il luogo del decesso  
+- Presenza di una fonte di calore nelle immediate vicinanze del corpo  
+- Presenza di riscaldamento a pavimento sotto il corpo  
+- Ipotermia accertata o sospetta (temperatura corporea iniziale inferiore a 35 °C)  
+- Impossibilità di determinare la temperatura ambientale media  
+- Impossibilità di stimare il fattore correttivo di Henssge  
+- Aumento significativo della temperatura ambientale (da valori bassi a elevati)  
+"""
+)
 
