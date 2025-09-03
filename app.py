d3173f34308778cb6bcba29a77eccdd8a67546f8
scopusjin <matteo.moretti19@gmail.com>
2025-09-03 18:59:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -539,7 +539,8 @@ def pannello_suggerisci_fc(peso_default: float = 70.0, key_prefix: str = "fcpane
             st.button("➕ Aggiungi a range FC",
                       use_container_width=True, on_click=add_fc_suggestion_global,
                       args=(result.fattore_finale,), key=k("btn_add_fc_imm"))
-            
+        st.session_state["fc_min_val"] = st.session_state.get("FC_min_beta")
+        st.session_state["fc_other_val"] = st.session_state.get("FC_max_beta")    
         return
 
 
