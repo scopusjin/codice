492be145affa12c474c59e7f03c11357f42b960a
scopusjin <matteo.moretti19@gmail.com>
2025-09-29 17:17:04 +0200

    Update App_MSIL.py

--- pages/App_MSIL.py
+++ pages/App_MSIL.py
@@ -591,5 +591,5 @@ if st.session_state.get("run_stima_mobile"):
 st.session_state["selettore_macchie"] = selettore_macchie
 st.session_state["selettore_rigidita"] = selettore_rigidita
 
-with st.popover("🚨 Raccomandazioni"):
+with st.popover("ℹ️ Raccomandazioni"):
     st.markdown(_raccomandazioni_html(), unsafe_allow_html=True)
