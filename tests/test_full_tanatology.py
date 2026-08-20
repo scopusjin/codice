82bb7df4fad366da82c519d12a38bc4fae692076
scopusjin <matteo.moretti19@gmail.com>
2026-08-21 00:10:26 +0200

    Use unittest for full tanatology mappings

--- tests/test_full_tanatology.py
+++ tests/test_full_tanatology.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import unittest
+
 from app.full_tanatology import (
     FULL_LIVOR_STATE_BY_LABEL,
     FULL_RIGOR_STATE_BY_LABEL,
@@ -9,13 +11,23 @@ from app.full_tanatology import (
 from app.parameters import opzioni_macchie, opzioni_rigidita
 
 
-def test_full_livor_ui_matches_current_parameter_options():
-    assert list(FULL_LIVOR_STATE_BY_LABEL.keys()) == list(opzioni_macchie.keys())
-    for ui_label in opzioni_macchie:
-        assert full_livor_legacy_value(ui_label) == ui_label
+class FullTanatologyMappingTests(unittest.TestCase):
+    def test_full_livor_ui_matches_current_parameter_options(self):
+        self.assertEqual(
+            list(FULL_LIVOR_STATE_BY_LABEL.keys()),
+            list(opzioni_macchie.keys()),
+        )
+        for ui_label in opzioni_macchie:
+            self.assertEqual(full_livor_legacy_value(ui_label), ui_label)
+
+    def test_full_rigor_ui_matches_current_parameter_options(self):
+        self.assertEqual(
+            list(FULL_RIGOR_STATE_BY_LABEL.keys()),
+            list(opzioni_rigidita.keys()),
+        )
+        for ui_label in opzioni_rigidita:
+            self.assertEqual(full_rigor_legacy_value(ui_label), ui_label)
 
 
-def test_full_rigor_ui_matches_current_parameter_options():
-    assert list(FULL_RIGOR_STATE_BY_LABEL.keys()) == list(opzioni_rigidita.keys())
-    for ui_label in opzioni_rigidita:
-        assert full_rigor_legacy_value(ui_label) == ui_label
+if __name__ == "__main__":
+    unittest.main()
