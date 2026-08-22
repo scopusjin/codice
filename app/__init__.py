6915fea54bd90fead4ff6d0da389d4f0d44e58ca
scopusjin <matteo.moretti19@gmail.com>
2026-08-22 20:25:46 +0200

    Install responsive perioral grid

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import streamlit as st
+
 import app.sopraciliare_ui as _sopraciliare_ui
+from app.perioral_single_grid import install_perioral_single_grid
 from app.special_heading_ui import install_special_heading_style
 from app.supra_single_grid import install_supra_single_grid
 
 install_supra_single_grid(_sopraciliare_ui)
+install_perioral_single_grid(_sopraciliare_ui)
 _sopraciliare_ui.install_sopraciliare_click_selector()
+
+_electrical_selectbox = st.selectbox
+
+
+def _selectbox_with_perioral_grid(label, options, *args, **kwargs):
+    if label == _sopraciliare_ui._PERIORAL_LABEL:
+        return _sopraciliare_ui._render_perioral_tile_grid(
+            widget_key=kwargs.get("key"),
+            options=list(options),
+        )
+    return _electrical_selectbox(label, options, *args, **kwargs)
+
+
+st.selectbox = _selectbox_with_perioral_grid
 install_special_heading_style()
