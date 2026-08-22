be7be1e94d42a0837ba566cd4ce10419bcfd413b
scopusjin <matteo.moretti19@gmail.com>
2026-08-22 20:00:54 +0200

    Install special parameter heading style

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import app.sopraciliare_ui as _sopraciliare_ui
+from app.special_heading_ui import install_special_heading_style
 from app.supra_single_grid import install_supra_single_grid
 
 install_supra_single_grid(_sopraciliare_ui)
 _sopraciliare_ui.install_sopraciliare_click_selector()
+install_special_heading_style()
